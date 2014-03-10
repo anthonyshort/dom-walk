eaa3c2e298448f684469b80862ec8f29166e3123
Anthony Short <antshort@gmail.com>
2014-03-09 17:18:05 -0700

    Nodes aren't walked if they aren't in the original node anymore

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var array = require('to-array');
 var contains = require('dom-contains');
 
-function walk(el, process, done) {
+function walk(el, process, done, root) {
+  root = root || el;
   var end = done || function(){};
   var nodes = array(el.childNodes);
 
   function next(){
     if(nodes.length === 0) return end();
     var nextNode = nodes.shift();
-    if(!contains(el, nextNode)) return next();
-    walk(nextNode, process, next);
+    if(!contains(root, nextNode)) return next();
+    walk(nextNode, process, next, root);
   }
 
   process(el, next);
